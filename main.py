07f59b7eecf29806dcc604817ca2323157b90c21
Binson40 <binsonjohn24@gmail.com>
2025-10-01 12:26:12 +0800

    5.Split data set into two tarin data and test data

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 #section for the Import the data set
 print('#section for the Import the data set')
 dataset = pd.read_csv('Data.csv')
@@ -27,12 +29,30 @@ print('#section for the encoding Independent variable')
 ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])], remainder='passthrough')
 x=np.array(ct.fit_transform(x))
 print(x)
-
 #section for the encoding dependent variable
 print('#section for the encoding dependent  variable')
 le= LabelEncoder()
 y=np.array(le.fit_transform(y))
 print(y)
+#Tarin and test data split
+print('#section for the Tarin and test data split')
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+print('#section for the X_Tarin and X_test data split')
+print(X_train)
+print(X_test)
+print('#section for the Y_Tarin and Y_Test data split')
+print(y_train)
+print(y_test)
+#Feature scaling Standard
+print('#section for the Feature scaling Standard  ')
+sc = StandardScaler()
+X_train[:,3:]=sc.fit_transform(X_train[:,3:])
+print( X_train)
+print( 'x-test----')
+X_test[:,3:]=sc.fit_transform(X_test[:,3:])
+print(X_test)
+
+
 
 
 
